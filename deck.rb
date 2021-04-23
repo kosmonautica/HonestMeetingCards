79a3de170d396e2f248dea71cfc9b4b39323c5ce
Udo Wiegärtner <kosmonaut@gmx.net>
2021-04-23 19:45:12 +0200

    first working deck 20 cards

--- deck.rb
+++ deck.rb
@@ -1,6 +1,39 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+require 'squib'
+
+data = Squib.xlsx file: 'CardContent.xlsx'
+layouts = ["hand.yml"]
+
+# ******* FRONT SIDES ******
+
+frontsides = Squib::Deck.new cards: 20, layout: layouts do
+  background color: 'white'
+    rect layout: 'cut' # cut line as defined by TheGameCrafter
+    rect layout: 'safe' # safe zone as defined by TheGameCrafter
+  
+  
+ text str: data['CardNumber'],
+	color: 'black',
+	x: 100,
+	y: 100
+	
+
+text str: data['TextFrontside'],
+	color: 'black',
+	font: 'Sans 30',
+	width: 650, height: 800,
+  ellipsize: :autoscale,
+	x: 100,	y: 200
+	
+text str: 'Honest Meeting Cards 1.0 / @UdoWiegaertner / CC BY NC SA',
+	color: 'black',
+	font: 'Sans 5',
+  x: 100,
+  y: 1000,
+  align: :center
+	# width: 800, height: 80,
+	# align: :center, y: 1000
+
+save_pdf file:'HonestMeetingCardsfrontsides.pdf'
 end
